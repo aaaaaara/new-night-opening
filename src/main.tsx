0fd30c23aa01cb616908bb9e9c48a8fad1754ff9
aaaaaara <araring.j@gmail.com>
2024-08-04 12:30:41 +0900

    core: add styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App.tsx';
+import GlobalStyles from './styles/GlobalStyles.ts';
+import Theme from './styles/Theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <ThemeProvider theme={Theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/Theme.ts
@@ -0,0 +1,10 @@
+const Theme = {
+  primary: '#003c72',
+  red: '#da120d', //
+  green: '#32DC76',
+  gray: '#f4f5f9', //disabled
+  black: '#121212',
+  white: '#fff',
+};
+
+export default Theme;
