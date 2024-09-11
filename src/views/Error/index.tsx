010a2d0cb6c20e1c78078873be1f38264020fd0c
aaaaaara <araring.j@gmail.com>
2024-09-11 20:57:13 +0900

    feat: add container style

--- src/views/Error/index.tsx
+++ src/views/Error/index.tsx
@@ -1,5 +1,17 @@
+import * as Styles from './index.styles';
+
+const ERROR_MESSAGE = {
+  ACCESS: `잘못된 접근입니다.`,
+  DEFAULT: ``,
+  SERVER: ``,
+};
+
 function ErrorView() {
-  return <div>ErrorView</div>;
+  return (
+    <Styles.Container>
+      <Styles.Content>{ERROR_MESSAGE.ACCESS}</Styles.Content>
+    </Styles.Container>
+  );
 }
 
 export default ErrorView;
