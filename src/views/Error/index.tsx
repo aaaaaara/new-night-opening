fae6aecffa63f179f80f965272f04570bdf9a295
aaaaaara <araring.j@gmail.com>
2024-09-16 18:29:27 +0900

    core: add tanstack query devtool & feat: 에러페이지 UI

--- src/views/Error/index.tsx
+++ src/views/Error/index.tsx
@@ -1,15 +1,49 @@
+import Typography from '../../components/Typography/Typography';
+import Theme from '../../styles/Theme';
 import * as Styles from './index.styles';
 
 const ERROR_MESSAGE = {
-  ACCESS: `잘못된 접근입니다.`,
-  DEFAULT: ``,
-  SERVER: ``,
+  ACCESS: `서비스에 접근할 수 없습니다.`,
+  NOT_FOUND: `페이지를 찾을 수 없습니다.`,
 };
 
 function ErrorView() {
   return (
     <Styles.Container>
-      <Styles.Content>{ERROR_MESSAGE.ACCESS}</Styles.Content>
+      <Styles.Content>
+        {/* TODO: 아이콘 컴포넌트로 변경*/}
+        <p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="65"
+            height="64"
+            viewBox="0 0 65 64"
+            fill="none"
+          >
+            <g clip-path="url(#clip0_104_114)">
+              <path
+                d="M32.5 58.6667C17.772 58.6667 5.83333 46.728 5.83333 32C5.83333 17.272 17.772 5.33333 32.5 5.33333C47.228 5.33333 59.1667 17.272 59.1667 32C59.1667 46.728 47.228 58.6667 32.5 58.6667ZM29.8333 40V45.3333H35.1667V40H29.8333ZM29.8333 18.6667V34.6667H35.1667V18.6667H29.8333Z"
+                fill="#003C72"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_104_114">
+                <rect
+                  width="64"
+                  height="64"
+                  fill="white"
+                  transform="translate(0.5)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </p>
+        <Typography
+          variant="heading3"
+          color={Theme.primary}
+          children={ERROR_MESSAGE.NOT_FOUND}
+        />
+      </Styles.Content>
     </Styles.Container>
   );
 }
